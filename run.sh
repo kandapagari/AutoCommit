c37a45d04b041addfd2d2d9f2943be78e48344bf
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2023-07-18 15:51:00 +0200

    feat(run.sh): update autocommit.py path

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 eval "$(conda shell.bash hook)"
 conda activate codecommit
 if [ $# -eq 0 ];
 then
-    ./src/autocommit.py
+    $(dirname "$0")/src/autocommit.py
     conda deactivate
     exit 0
 fi
