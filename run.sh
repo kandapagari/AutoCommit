e1e3328e04c8c774e617e8aabc717b2d3dc68ee3
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2023-07-18 16:08:31 +0200

    feat(run.sh): modify code to pass command line args
    fix(autocommit.py): fix typo in error message

--- run.sh
+++ run.sh
@@ -2,11 +2,5 @@
 
 eval "$(conda shell.bash hook)"
 conda activate codecommit
-if [ $# -eq 0 ];
-then
-    $(dirname "$0")/src/autocommit.py
-    conda deactivate
-    exit 0
-fi
+$(dirname "$0")/src/autocommit.py "$@"
 conda deactivate
-exit 0
