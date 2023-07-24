029e439c140446043aae09c1f3a1ffeb03172d53
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2023-07-24 11:30:44 +0200

    feat(run.sh): update error message for missing autocommit env

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ find_in_conda_env(){
 if find_in_conda_env ".*autocommit.*" ; then
     conda activate autocommit
 else
-    echo "autocommit not found creating it"
+    echo "autocommit env not found creating it"
     conda create -n autocommit python=3.10 -y
     conda activate autocommit
     pip install poetry
